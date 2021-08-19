b286966d95220a91eaf2ddd38aa4719a391da964
Josiah <josiah.pilon@yahoo.ca>
2021-08-19 14:01:41 -0700

    moved logic that pertained to users

--- express-back-end/dist/index.js
+++ express-back-end/dist/index.js
@@ -3,58 +3,6 @@ import pool from "./db/index";
 
 const App = Express();
 
-
-
-
-
-const getUserByUserId = (id) => {
-    const query = `
-  SELECT users.*,
-  fonts.script as body_script,
-  title.script as title_script
-  FROM users 
-  JOIN fonts ON users.body_font_id = fonts.id 
-  JOIN (
-    SELECT fonts.*, users.id as user_id
-    FROM users 
-    JOIN fonts ON users.title_font_id = fonts.id 
-    WHERE users.id = $1
-  ) as title
-  ON users.id = title.user_id
-    WHERE users.id = $1;`;
-    const queryParams = [id];
-    return pool.query(query, queryParams);
-};
-const getUsers = () => {
-    const query = 'SELECT * FROM users';
-    return pool.query(query);
-};
-
-const insertIntoDatabase = (attributes, table) => {
-    const queryParams = [];
-    let queryStart = `INSERT INTO ${table} (`;
-    let queryMid = ') VALUES (';
-    let queryEnd = ') RETURNING id';
-    for (const [attribute, value] of Object.entries(attributes)) {
-        if (queryParams.length) {
-            queryStart += ', ';
-            queryMid += ', ';
-        }
-        queryParams.push(value);
-        queryStart += `${attribute}`;
-        queryMid += `$${queryParams.length}`;
-    }
-    if (table !== 'users') {
-        queryEnd += ', user_id as userId';
-    }
-    const queryString = queryStart + queryMid + queryEnd;
-    return pool.query(queryString, queryParams);
-};
-
-
-const insertUser = (attributes) => {
-    return insertIntoDatabase(attributes, 'users');
-};
 const updateDatabase = (attributes, identifiers) => {
     const { table, type, id } = identifiers;
     const queryParams = [];
